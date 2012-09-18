5aa409ebdca4eda5332bd6ecef4df07de42d4540
Geoff Low <glow@mdsol.com>
2012-09-18 10:35:32 +0100

    Added rspec to project

---
+++ spec/lib/atomic_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Atomic do
+
+  it "should get a list of branches for a repo" do
+    pending
+  end
+  
+  it "should get a list of commits for a branch on a repo" do
+    pending
+  end
+  
+  it "should generate a list of branches on one repository, but not the other" do
+    pending
+  end
+
+  it "should generate a list of commits on one repository branch, but not the other" do
+    pending
+  end
+  
+  it "should cache the lookups on the source repository" do
+    pending
+  end
+  
+end
