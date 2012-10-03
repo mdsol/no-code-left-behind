d75827785af9b9ebc9ab3cad865405e5d8921877
Geoff Low <glow@mdsol.com>
2012-10-03 15:15:12 +0100

    factored clone_repository into it's own method added notification calls added relative_require fixed method is_fork in repaired duplicated merge_fork fixed error in synonym method

--- lib/nuclear.rb
+++ lib/nuclear.rb
@@ -1,8 +1,6 @@
 require 'octokit'
 require 'grit'
-require 'blockable'
-
-include Grit
+require_relative './blockable'
 
 =begin
 [REMOTE]Need to use the github api to create the fork
@@ -28,9 +26,9 @@ class KitchenDrawer
   def merge_fork(forked_repository)
     # intended to be called multiple times
     # name of forked repo
-    if self.is_fork(forked_repository)
+    if self.is_fork?(forked_repository)
       self.get_or_create_fork(forked_repository)
-      self.merge_fork(forked_repository)
+      self.merge_forked_repository(forked_repository)
       self.complete_merge?(forked_repository)
     else
       raise FissionError, "#{forked_repository} is not a fork"
@@ -59,6 +57,7 @@ class KitchenDrawer
     source_repo = self.source_repo(repository_name)
     my_repos = self.client.repositories().collect {|x| x[:name]}
     unless my_repos.include?(source_repo[:name])
+      puts "Forking #{source_repo[:full_name]}"
       self.client.fork(source_repo[:full_name])
     end
     # return repository object
@@ -66,7 +65,7 @@ class KitchenDrawer
     self.client.repository("#{self.client.login}/#{source_repo[:name]}")
   end
   
-  def merge_fork(fork_repository)
+  def merge_forked_repository(fork_repository)
     # TODO: refactor this - massive function, but the grit deps are a pain
     
     # retrieve the repo hashes
@@ -75,47 +74,40 @@ class KitchenDrawer
     dep_repo = self.my_fork(source_repo[:name])
     
     # synonyms for clarity
-    fork_owner = fork_owner[:owner][:login]
+    fork_owner = fork_repo[:owner][:login]
     departed_user = self.client.login
     
     # if the clone already exists, then reuse it, otherwise clone (can take a long time)
     unless Dir.exist?(File.join(@tmplocation, source_repo[:name]))
-      begin
-        # need to clone the directory
-        gitty = Grit::Git.new(@tmplocation)
-        # clone of some projects can take a while, I'm thinking of you R...
-        # timeout is in seconds - allow 40m to clone
-        gitty.clone({:quiet => false, 
-                    :verbose => true, 
-                    :progress => true,
-                    :timeout => @timeout}, 
-                    source_repo[:ssh_url], 
-                    File.join(@tmplocation, source_repo[:name]))
-      rescue Git::Timeout
-        raise FissionError, "Timeout #{@timeout} exceeded on clone, increase and rerun"
-      end
+      self.clone_repository(source_repo)
     end
+    
     # Repo object
     g = Grit::Repo.new(File.join(@tmplocation, source_repo[:name]))
-    #g = Git.clone(@dep_repository["clone_url"], :name => 'name', :path => tmp_dir)
+    
     # add a remote for the fork_owner
     g.remote_add(fork_owner, fork_repo[:ssh_url])
     # add a remote for the departed_user
     g.remote_add(departed_user, dep_repo[:ssh_url])
+    
     # fetch the forked repo
     begin
       g.git.fetch({:timeout => @timeout}, fork_owner)
     rescue Git::Timeout
       raise FissionError, "Timeout #{@timeout} exceeded on fetch, increase and rerun"
     end  
+    remote_branches = self.client.branches(dep_repo[:full_name]).collect {|x| x[:name]}
     # iterate through the branches on the local 
     g.remotes.select {|x| x.name.start_with?(fork_owner)}.each do |branch|
       # cloned branch {leaver}_{branch}, eg someUser_develop, someUser_feature/sausages
       # branch.full is like remotes/someUser/
-      cloned_branch = self.client.login + "_" + branch.full.split("/")[2..-1].join("/")
+      cloned_branch = branch.name.gsub("#{fork_owner}/", "#{fork_owner}_")
       # using GH api here
-      remote_branches = self.client.branches(dep_repo[:full_name]).collect {|x| x[:name]}
-      unless remote_branches.include?(cloned_branch)
+      # only create and push if the branch doesn't already exist
+      if remote_branches.include?(cloned_branch)
+        puts "#{cloned_branch} already merged"
+      else
+        puts "Cloning #{branch.name} as #{cloned_branch}"
         # create the branch as "{leaver}_{branch_name}"
         # create the branch on the local copy
         g.git.branch({}, cloned_branch, branch.name)
@@ -130,4 +122,22 @@ class KitchenDrawer
      
   end
   
+  def clone_repository(repository)
+    # clone repository to local system
+    begin
+      puts "Creating local clone of source #{repository[:full_name]}"
+      # need to clone the directory
+      gitty = Grit::Git.new(@tmplocation)
+      # clone of some projects can take a while, I'm thinking of you R...
+      # timeout is in seconds - allow 40m to clone
+      gitty.clone({:quiet => false, 
+                  :verbose => true, 
+                  :progress => true,
+                  :timeout => @timeout}, 
+                  repository[:ssh_url], 
+                  File.join(@tmplocation, repository[:name]))
+    rescue Git::Timeout
+      raise FissionError, "Timeout #{@timeout} exceeded on clone, increase and rerun"
+    end
+  end
 end
