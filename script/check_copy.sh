4bd851e4a4e088648f72014c4ea4d3b5f8b701d6
Geoff Low <glow@mdsol.com>
2012-11-07 10:43:48 +0000

    added check_copy.sh

---
+++ script/check_copy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Pass in DEPARTEDUSER as the environment variable
+
+function check_hashes
+{
+  local forkname=$1
+  local repository=$(echo ${forkname#*/})
+  local owner=$(echo ${forkname%/*})
+  local fork="${owner}/${repository}"
+  local copy="${DEPARTEDUSER}/${repository}"
+  copy_branches=$(curl https://api.github.com/repos/${copy}/branches)
+  shas=$(curl https://api.github.com/repos/${fork}/branches | grep '"sha"' | awk '{print $2}') 
+  for sha in ${shas}; 
+  do 
+    echo "Checking ${sha}" 
+    check=$(echo $copy_branches | grep $sha); 
+    if [ -z "${check}" ]; then 
+      echo "${shas} is missing"; 
+    fi; 
+  done
+}
+
+check_hashes $1
